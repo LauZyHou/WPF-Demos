95b460dc9e241315e3090c50d2cccd0619c5601d
LauZyHou <java233@foxmail.com>
2019-10-31 21:50:26 +0800

    控件

---
+++ control/ItemsControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Demos.control
+{
+    /// <summary>
+    /// ItemsControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class ItemsControl : Window
+    {
+        public ItemsControl()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonVictor_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            DependencyObject level1 = VisualTreeHelper.GetParent(btn);
+            DependencyObject level2 = VisualTreeHelper.GetParent(level1);
+            DependencyObject level3 = VisualTreeHelper.GetParent(level2);
+            MessageBox.Show(level3.GetType().ToString());
+        }
+    }
+}
